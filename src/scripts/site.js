71f733c4d3e2e3588750f564b1e199267e454a8b
omnikrll <kfhohn@gmail.com>
2016-01-06 19:29:36 -0500

    define home hash list outside of checkHash scope

--- src/scripts/site.js
+++ src/scripts/site.js
@@ -1,7 +1,7 @@
+var homeHashes = ['', '#', '#home', '#index'];
+
 var checkHash = function() {
-	console.log(window.location.hash, typeof window.location.hash);
-	var homeHashes = ['', '#', '#home', '#index'],
-		hash = homeHashes.indexOf(window.location.hash) != -1 ? '#home' : window.location.hash;
+	var hash = homeHashes.indexOf(window.location.hash) != -1 ? '#home' : window.location.hash;
 
 	if (!!hash) {
 		var page = '../src/content/' + hash.replace('#', '') + '.html';
